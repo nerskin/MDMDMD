6d6686dfaf78db864e7275c251226ffdf0338736
swanepoelmarcel <110138819+swanepoelmarcel@users.noreply.github.com>
2022-07-29 13:03:38 +1000

    App

--- Traffic_Cam_Shiny.R
+++ Traffic_Cam_Shiny.R
@@ -8,34 +8,34 @@
 #
 
 library(shiny)
-
+library(ggplot2)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
     titlePanel("Traffic Camera Offence and Fines"),
-
-    # Sidebar with a slider input for number of bins 
-selectInput("var",label = "Option 1", choices = list("State of Vehicle Registration", "Vehicle Category","Camera Type"))
-)
-
+    sidebarLayout(
+      sidebarPanel(
+selectInput("var",
+            label = "Choose X Axis", 
+            choices = list("State of Vehicle Registration", "Vehicle Category","Camera Type"), 
+            selected = "Camera Type")
+),
 mainPanel(
+  plotOutput("plot")
 )
-
+)
+)
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   df <- read_csv("Traffic_camera_offences_and_fines.csv")
 
-  output$plot <- renderPlot({Switch(input$var, 
-                                    "" = ggplot(df,aes())+ 
-                                      geom_col
-                                    
-                                    
-                                    )})
-  plot <- ggplot(df,aes(x = Clt_Catg, y = Sum_Pen_Amt)) + 
-    geom_col()
   
-  plot
+  output$plot <- renderPlot({ggplot(df,aes(x = switch(input$var,"State of Vehicle Registration" = Rego_State, "Vehicle Category" = Clt_Catg, "Camera Type" = Camera_Type),
+                                             y = Sum_Pen_Amt)) +
+                                      geom_col()
+                                    
+  })
 }
 
 # Run the application 
